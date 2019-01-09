b373f3deb7e20371523cd357518f4385c0e24ece
nabuchi <nabnab.main+github@gmail.com>
2019-01-09 19:49:36 +0900

    property test sample

--- src/test/kotlin/net/nabnet/test_sample/TestSampleApplicationTests.kt
+++ src/test/kotlin/net/nabnet/test_sample/TestSampleApplicationTests.kt
@@ -4,13 +4,17 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.beans.factory.annotation.Value
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class TestSampleApplicationTests {
+	@Value("\${url}")
+	lateinit var url: String
 
 	@Test
-	fun contextLoads() {
+	fun testSample() {
+		throw RuntimeException(url)
 	}
 
 }
