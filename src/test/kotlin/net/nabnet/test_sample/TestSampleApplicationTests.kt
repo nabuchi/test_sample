0b5826d552e22a6916f3b7e5e03e07ceb0116ac5
nabuchi <nabnab.main+github@gmail.com>
2019-01-09 20:04:05 +0900

    TestPropertySource

--- src/test/kotlin/net/nabnet/test_sample/TestSampleApplicationTests.kt
+++ src/test/kotlin/net/nabnet/test_sample/TestSampleApplicationTests.kt
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.test.context.TestPropertySource
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@TestPropertySource(locations = ["/test.properties"])
 class TestSampleApplicationTests {
-	@Value("\${url}")
+	@Value("\${aaa}")
 	lateinit var url: String
 
 	@Test
